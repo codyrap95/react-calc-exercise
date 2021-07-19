909f3af3ef131171ff0af6666543792386392366
Cody Dubat <codyrap95@yahoo.com>
2021-07-19 23:28:24 +0300

    Calculator Logic Works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Calculator from "./components/Calculator";
 
 function App() {
+  const [firstValue, setFirstValue] = useState("");
+  const [secondValue, setSecondValue] = useState("");
+  const [signValue, setSignValue] = useState("");
+  const [result, setResult] = useState("");
+  const calculate = (first: number, second: number, signValue: string) => {
+    if (signValue === "") return;
+    if (signValue === "+") return setResult((first + second).toString());
+    if (signValue === "-") return setResult((first - second).toString());
+  };
+  const passInputHandler = (inputData: any) => {
+    if (inputData.type === "=" && firstValue !== "" && secondValue !== "") {
+      let first: number = parseInt(firstValue);
+      let second: number = parseInt(secondValue);
+      calculate(first, second, signValue);
+      return;
+    }
+    if (firstValue === "" && signValue === "") {
+      setFirstValue(inputData.value);
+      setSignValue(inputData.type);
+      return;
+    }
+    setSecondValue(inputData.value);
+    if (inputData.type !== "=") setSignValue(inputData.type);
+  };
+  const clearHandler = () => {
+    setFirstValue("");
+    setSecondValue("");
+    setSignValue("");
+    setResult("");
+  };
   return (
     <React.Fragment>
-      <Calculator />
+      <Calculator passInput={passInputHandler} />
+      <p>
+        {firstValue} {signValue} {secondValue}
+      </p>
+      <p>Result: {result}</p>
+      <button onClick={clearHandler}>Clear</button>
     </React.Fragment>
   );
 }
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,27 +1,39 @@
 import React, { useRef } from "react";
 import classes from "./Calculator.module.css";
 
-export default function Calculator() {
+export default function Calculator(props: any) {
   const inputRef = useRef<HTMLInputElement>(null);
-  const plusHandler = () => {};
-  const minusHandler = () => {};
+  const plusHandler = () => {
+    props.passInput({ value: inputRef.current?.value, type: "+" });
+  };
+  const minusHandler = () => {
+    props.passInput({ value: inputRef.current?.value, type: "-" });
+  };
   const submitHandler = (event: any) => {
     event.preventDefault();
-    console.log(inputRef.current?.value);
+    props.passInput({ value: inputRef.current?.value, type: "=" });
+    console.log("arrived at submit handler");
   };
   return (
-    <form onSubmit={submitHandler} className={classes.calculatorWrapper}>
-      <label htmlFor="calculator-input">Type Here:</label>
-      <input
-        type="number"
-        name="calculator-input"
-        id="calculator-input"
-        min={0}
-        ref={inputRef}
-      />
-      <button onClick={plusHandler}>+</button>
-      <button onClick={minusHandler}>-</button>
-      <button type="submit">=</button>
-    </form>
+    <div className={classes.calculatorWrapper}>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="calculator-input">Type Here:</label>
+        <input
+          type="number"
+          name="calculator-input"
+          id="calculator-input"
+          min={0}
+          step={1}
+          ref={inputRef}
+        />
+        <button type="button" onClick={plusHandler}>
+          +
+        </button>
+        <button type="button" onClick={minusHandler}>
+          -
+        </button>
+        <button type="submit">=</button>
+      </form>
+    </div>
   );
 }

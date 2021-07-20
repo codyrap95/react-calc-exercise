2fb93744b8aac4d05a79e1f603e0d9926e5c77b2
Cody <codydjay@gmail.com>
2021-07-20 13:25:36 +0300

    Second try with axios

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,7 @@
 import React, { useReducer } from "react";
+import axios from "axios";
+
+let api = axios.create({ baseUrl: "https://swapi.dev/api/films" });
 
 const numReducer = (state, action) => {
   if (action.type === "INCREASE") return state + 1;
@@ -8,8 +11,17 @@ const numReducer = (state, action) => {
 export default function Counter(props) {
   const [numState, dispatch] = useReducer(numReducer, 0);
   const fetchSW = async () => {
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    let data = [];
+    try {
+      data = await api
+        .get("https://swapi.dev/api/films")
+        .then((res) => res.data);
+    } catch (e) {
+      console.log(e);
+    }
+    // const response = await fetch("https://swapi.dev/api/films");
+    // const data = await response.json();
+    console.log(data);
     props.fetchedData(data.results);
   };
   return (
